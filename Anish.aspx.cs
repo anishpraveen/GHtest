e7709c62337d94a7c1134ef7ac2173ac9da0f1c0
anishpraveen <anishpraveen7@gmail.com>
2015-07-07 00:03:53 +0530

    First Commit

---
+++ Anish.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Anish : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void bttnHello_Click(object sender, EventArgs e)
+    {
+        lblHello.Text ="Hi, guys time is:" + Convert.ToString(System.DateTime.Now);
+        lblHello.Visible = true;
+        
+    }
+}
